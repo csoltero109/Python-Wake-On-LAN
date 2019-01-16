33467175a2d606f0044d982249a0b53e37ac549a
csoltero109 <you@example.com>
2019-01-16 01:49:58 +0000

    First push to Github

---
+++ GUI.py
@@ -0,0 +1,22 @@
+from wakeonlan import send_magic_packet
+from tkinter import *
+
+class RemoteStart:
+    def __init__(self,master):
+        frame = Frame(master)
+        frame.pack()
+        
+        self.startUp = Button(frame, text = "Wake Up Server", command = self.turnOnServer)
+        self.startUp.pack(side = LEFT)
+        
+        self.close = = Button(frame, text = "Closing Prompt", command = frame.quit)
+        self.close.pack(side = LEFT)
+        
+    def turnOnServer(self):
+        print("Turning On Server")
+        send_magic_packet('ff.ff.ff.ff.ff.ff')
+        
+root = Tk()
+b = RemoteStart(root)
+root.mainloop()
+        
